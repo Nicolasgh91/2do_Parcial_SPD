9c7fdedc8e29681fcba8c4a76dca7c6191048e68
Nicolasgh91 <gulley.erase0h@icloud.com>
2023-07-10 09:17:46 -0300

    Modifico código y agrego opción de setear las estaciones del año mediante el control remoto

--- codigo/codigo.ino
+++ codigo/codigo.ino
@@ -35,9 +35,12 @@ LiquidCrystal lcd(7,6,5,4,3,2);
 
 //INICIO Variables
 int angulo_servo = 0;
-int numero_presionado;
+int numero_presionado; // Variable global para almacenar el "boton" presionado en el control
 bool flag_incendio = false;
+bool flagEncendido = false;
+String estacionElegida = "";
 //FIN Variables
+
 
 
 
@@ -95,127 +98,121 @@ int recibirNumero()
   		}
 	return digito; // Guardo el resultado
 } 
+
+
+
+// Imprime la estación del año en el display lcd
+void estacionDelAnio(String estacion)
+{	
+  //lcd.setCursor(9,1);
+  if (estacion == "invierno")
+  {
+    lcd.print("Invierno");
+  }
+  else if (estacion == "otonio")
+  {
+    lcd.print("Otono    ");
+  }
+  else if (estacion == "primavera")
+  {
+    lcd.print("Primavera");
+  } 
+  else if (estacion == "verano")
+  {
+    lcd.print("Verano   ");
+  }
+}
 //FIN Funciones
+
 
 
 
 //INICIO LOOP PRINCIPAL
 void loop()
 {
-  
   //Las mediciones del sensor de temperatura se realizan de manera continua, por ende las variables deben ir en el loop principal para poder proporcionar un valor lo más acertado posible.
   int lectura_sens_temp = analogRead(SENSOR_TEMP); //Capto la lectura del sensor
-  float voltaje = lectura_sens_temp * (5000 / 1024.0); // Convierto la lectura a voltaje
-  float temp_celcius = (voltaje - 500) / 10; // Convierto el voltaje a grado celcius
+  int temp_celcius = map(lectura_sens_temp, 20, 358, -40, 125);
   //Fin sensor de temperatura
  
-  
   
   //Configuración sensor IR y control
   if(IrReceiver.decode())// Su valor constante es 0, pero si algún botón es presionado se convierte en 1 y pasa a ser "true". Cuando es "true" pasa el valor hexadecimal que le corresponde al botón según el control remoto
   {          
     numero_presionado = recibirNumero(); // Ese valor hexa se lo paso a la función que de acuerdo a las constantes definidas al principio del programa, me indicaran que valor númerico equivale a tal botón.
+    
+  	if (numero_presionado == 0) // Apago
+   	{
+        lcd.clear(); // Limpio la pantalla del led
+      	digitalWrite(LED_NARANJA, 0);
+      	digitalWrite(LED_AZUL, 0);
+      	lcd.print(" ");
+      	flagEncendido = false;
+    }
+    else if (numero_presionado == 1) // Prendo
+   	{
+        lcd.clear(); // Limpio la pantalla del led
+      	digitalWrite(LED_NARANJA, 0);
+      	digitalWrite(LED_AZUL, 1); 
+      	flagEncendido = true;
+    }
+
+    if (numero_presionado == 2)
+   	{
+ 		estacionElegida = "invierno";
+    }
+    else if (numero_presionado == 3)
+   	{	
+      estacionElegida = "otonio";
+    }
+    else if (numero_presionado == 4)
+   	{
+ 		estacionElegida = "primavera";
+    }
+    else if (numero_presionado == 5)
+   	{
+ 		estacionElegida = "verano";
+    }
+    estacionDelAnio(estacionElegida);
+    
+    
     IrReceiver.resume(); // Esto le permite "reiniciar" su estado al sensor infrarrojo y dejarlo listo para "escuchar" otra señal.
-    
-
-    //Desactivo el sistema anti incendios (simulado con el servo motor) a pedido del usuario
-    if (numero_presionado == 0 && flag_incendio == true) //Si se presionó el botón 0 en el control y se detectó previamente una temperatura por encima de los 45°C
-    {
-    	servo_alarma.write(180); //Activo el motor y gira a 180°
-      	lcd.clear(); // Limpio la pantalla del led
-      	lcd.setCursor(0,0); //Preparo al LCD para poder imprimir texto en la posición x=0 e y=0
-      	lcd.print("Sistema"); //Este texto va a la posición previamente definida
-      	lcd.setCursor(0,1); //Ahora le paso para que imprima en la siguiente linea del LCD
-    	lcd.print("desactivado");
-      	digitalWrite(LED_NARANJA, 0); //Apago el LED indicador que se encendió previamente al detectarse una temperatura superior a los 45° C
-    }
-   
-    
-
-    if (numero_presionado == 1) //Si se presionó el botón 1 en el control remoto, el usuario activa nuevamente el sistema.
-    {	
-    	lcd.clear(); //Limpio pantalla
-      	lcd.setCursor(0,0);
-      	lcd.print("Sistema");
-      	lcd.setCursor(0,1);
-    	lcd.print("activado");
-      	delay(1000);
-    	flag_incendio = false; //Bajo la bandera de incendio, pero la temperatura seguirá siendo la misma.
-    }
   }
-
   
   
-  lcd.setCursor(0,0); //Limpio pantalla del LCD
-  
-  while (flag_incendio == false) //Mientras no haya incendio
+  if (flagEncendido == false)
   {
-    if (temp_celcius < 45)
-    {
-    lcd.print("Temperatura");
-    digitalWrite(LED_AZUL, 1);
-  	digitalWrite(LED_NARANJA, 0);
-    }
-
-    if(temp_celcius <= 9)//Invierno (Se simulan de esta manera las estaciones, ya que no hay un reloj de sistema en Arduino, por lo cual no hay manera simple de determinar la fecha y hora)
-    {
-      Serial.println("Temperatura invernal");
-      lcd.setCursor(0,1);
-      lcd.print("invernal ");
-
-    } 
-    else if (temp_celcius > 9 && temp_celcius < 17)//Otoño (Se simulan de esta manera las estaciones, ya que no hay un reloj de sistema en Arduino, por lo cual no hay manera simple de determinar la fecha y hora)
-    {
-      Serial.println("Temperatura otoñal");
-      lcd.setCursor(0,1);
-      lcd.print("otonal ");
-
-    } 
-    else if (temp_celcius >= 17 && temp_celcius <= 24)//Primavera (Se simulan de esta manera las estaciones, ya que no hay un reloj de sistema en Arduino, por lo cual no hay manera simple de determinar la fecha y hora)
-    { 
-      Serial.println("Temperatura primaveral");
-      lcd.setCursor(0,1);
-      lcd.print("primaveral ");
-
-    } 
-    else if (temp_celcius > 24 && temp_celcius < 45)//Verano (Se simulan de esta manera las estaciones, ya que no hay un reloj de sistema en Arduino, por lo cual no hay manera simple de determinar la fecha y hora)
-    {
-      Serial.println("Temperatura veraniega");
-      servo_alarma.write(180);
-      Serial.println("Servo en 180");
-      lcd.setCursor(0,1);
-      lcd.print("veraniega ");
-
-    } 
-    else if (temp_celcius >= 45 && temp_celcius < 60)// En este rango de temperatura, se advierte al usuario.
-    {
-      Serial.println("Posible incendio");
-      digitalWrite(LED_NARANJA, 1);
-      digitalWrite(LED_AZUL, 0);
-      lcd.setCursor(0,0);
-      lcd.print("POSIBLE");
-      lcd.setCursor(0,1);
-      lcd.print("INCENDIO ");
-    }
-    else if (temp_celcius >= 60) //Se activa el sistema de protección contra incendios.
-    {
-
-      if (flag_incendio == false) //Si no hubo incendio previo
-      {
-          servo_alarma.write(90); //Simulación sistema de respuesta anti incendios.
-          Serial.println("Servo en 90°");
-          flag_incendio = true; 
-      }
-
-      digitalWrite(LED_NARANJA, 1);
-      digitalWrite(LED_AZUL, 0);
-      lcd.setCursor(0,0);
-      lcd.print("SISTEMA ANTI");
-      lcd.setCursor(0,1);
-      lcd.print("INCENDIO ");
-    }
-    lcd.print(temp_celcius);
+    lcd.clear();
   }
-
+  else 
+  {
+    lcd.setCursor(0,0);
+    lcd.print(estacionElegida);
+    lcd.setCursor(11,0);
+    lcd.print(temp_celcius);
+    delay(300);
+  
+    if (temp_celcius >= 60) //Se activa el sistema de protección contra incendios.
+    {
+		servo_alarma.write(90); //Simulación sistema de respuesta anti incendios.  
+        flag_incendio = true;   
+        digitalWrite(LED_NARANJA, 1);
+        digitalWrite(LED_AZUL, 0);
+      	lcd.clear();
+        lcd.setCursor(11,0);
+   		lcd.print(temp_celcius);
+        lcd.setCursor(0,1);
+        lcd.print("INCENDIO ");
+      	
+    }
+    else
+    {
+      	servo_alarma.write(180);
+      	lcd.clear();
+    	flag_incendio == false;
+    	digitalWrite(LED_NARANJA, 0);
+        digitalWrite(LED_AZUL, 1);
+    }
+  }
 }
 //FIN LOOP PRINCIPAL
